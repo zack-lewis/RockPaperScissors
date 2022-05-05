b87da5ee721c1c270dbeb378a5ff37ed02668139
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-05-05 14:41:30 -0500

    Added library methods

--- RPS_Const.cs
+++ RPS_Const.cs
@@ -1,9 +1,57 @@
+using System;
+using System.Collections.Generic;
+
 namespace RockPaperScissors
 {
     static class RPS_Const
     {
         public static string playerLogFile = "player_log.csv";
-        public static string transactionLog = "playerTransactionLog.csv";
         public static string appLog = "RockPaperScissors.log";
+        public static string currentPlayerName = "";
+        public static List<Player> players = new List<Player>();
+        public static Player currentPlayer;
+
+        public static void consoleSendLine(string message, bool newLine = true, bool lineHeading = true) {
+            if(lineHeading) {
+                Console.Write("<<< ");
+            }
+            
+            Console.Write(message);
+
+            if(newLine) {
+                Console.Write("\n");
+            }
+        }
+    
+        public static void showMenu(List<string> stringList) {
+            const string border = "-";
+            int borderWidth = 0;
+                        
+            foreach(string s in stringList) {
+                borderWidth = Math.Max(borderWidth, s.Length + 6);
+            }
+
+            for(int i = 0; i < borderWidth; i++) {
+                RPS_Const.consoleSendLine(border,false,false);
+            }
+
+            RPS_Const.consoleSendLine("",true,false);
+
+            foreach(string s in stringList) {
+                int sLen = s.Length + 4;
+                RPS_Const.consoleSendLine($"| { s }",false,false);
+                for(int i = 0; i < borderWidth - sLen; i++) {
+                    RPS_Const.consoleSendLine(" ",false,false);
+                }
+                RPS_Const.consoleSendLine(" |",true,false);
+            }
+            
+            for(int i = 0; i < borderWidth; i++) {
+                RPS_Const.consoleSendLine(border,false,false);
+            }
+            RPS_Const.consoleSendLine("\n",false,false);
+
+        }
     }
+
 }
